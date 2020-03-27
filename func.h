d7aed4a19115350b3db574e3e83b4382283766f9
Kobelskiy Vadim <kobelskiy73@gmail.com>
2020-03-27 15:18:47 +0200

    Write code for strukture stack

--- func.h
+++ func.h
@@ -6,3 +6,13 @@ using namespace std;
 namespace funk {
 
 }
+namespace st {
+	struct stack;
+	void init(struct stack* stk);
+	void push(struct stack* stk, float);
+	float pop(struct stack* stk);
+	float stkTop(struct stack* stk);
+	int getcount(struct stack* stk);
+	int isempty(struct stack* stk);
+	void stkPrint(struct stack* stk);
+}
